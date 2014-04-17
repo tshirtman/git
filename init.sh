55797bf42974537764ddccf1b65d14eab6f3f623
tshirtman <gabriel.pettier@gmail.com>
2014-04-17 17:28:34 +0200

    fix init script

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ while true
 do
 read -p "do you want to link gitconfig to ~/.gitconfig (Y/N)" yn
 case $yn in
-	[Yy]*) ln -s ~/.git/gitconfig ~/.gitconfig; break;;
+	[Yy]*) ln -sf ~/git/gitconfig ~/.gitconfig; break;;
 	[Nn]*) break;;
 	*) echo "please answer yes or no";;
 esac
@@ -15,7 +15,7 @@ while true
 do
 read -p "do you want to link .gitignore_global to ~/.gitignore_global (Y/N)" yn
 case $yn in
-	[Yy]*) ln -s ~/.git/.gitignore_global ~/.gitignore_global; break;;
+		[Yy]*) ln -sf ~/.git/.gitignore_global ~/.gitignore_global; break;;
 	[Nn]*) break;;
 	*) echo "please answer yes or no";;
 esac
